de0ed83f5591a6561964d96d59091b7579cbb87d
Dawit Melka <davebadeg@gmail.com>
2023-04-10 23:23:44 +0300

    user login, logout and flash messages

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Project
 from .forms import ProjectForm
 # Create your views here.
@@ -15,6 +17,7 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'projectobj':projectObj,})
 
+@login_required(login_url="login")
 def createProject(request):
     form  = ProjectForm()
 
@@ -27,6 +30,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form  = ProjectForm(instance=project)
@@ -40,6 +44,7 @@ def updateProject(request, pk):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
